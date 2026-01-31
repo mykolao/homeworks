8d8a4a10d5af0fed316681472a631c77c6d1b097
mykolao <mykolaoleskiv@gmail.com>
2026-01-31 21:11:55 +0200

    complete task 8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,16 +4,29 @@ type ActionType =
   | { type: 'sort'; payload: 'up' | 'down' }
   | { type: 'check'; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => {
+const collator = new Intl.Collator('ru', {
+  sensitivity: 'base',
+  caseFirst: 'false',
+});
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => {
   // need to fix any
   switch (action.type) {
     case 'sort': {
       // by name
+      const order = action.payload;
+      const sorted = [
+        ...state.sort((a, b) => {
+          return collator.compare(a.name, b.name);
+        }),
+      ];
 
-      return state; // need to fix
+      return order === 'up' ? sorted : sorted.reverse();
     }
     case 'check': {
-      return state; // need to fix
+      const age = action.payload;
+      const filtered = state.filter((user) => user.age >= age);
+      return filtered;
     }
     default:
       return state;
