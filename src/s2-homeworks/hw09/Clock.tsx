922f2108be58f8da3c304be3fe6c5356f05aa266
mykolao <mykolaoleskiv@gmail.com>
2026-02-07 05:39:20 +0200

    complete task 8

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -14,25 +14,39 @@ function Clock() {
   const start = () => {
     // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
     // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
+    const id = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+    setTimerId(+id);
   };
 
   const stop = () => {
-    // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
+    clearInterval(timerId);
+    setTimerId(undefined);
   };
 
   const onMouseEnter = () => {
-    // пишут студенты // показать дату если наведена мышка
+    setShow(true);
   };
   const onMouseLeave = () => {
-    // пишут студенты // спрятать дату если мышка не наведена
+    setShow(false);
   };
 
-  const stringTime = 'date->time' || <br />; // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-  const stringDate = 'date->date' || <br />; // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+  const stringTime = new Intl.DateTimeFormat('en-GB', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  }).format(date) || <br />;
 
-  // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-  const stringDay = 'date->day' || <br />; // пишут студенты
-  const stringMonth = 'date->month' || <br />; // пишут студенты
+  const stringDate = new Intl.DateTimeFormat('en-GB').format(date) || <br />;
+
+  const stringDay = new Intl.DateTimeFormat('en-GB', {
+    weekday: 'long',
+  }).format(date) || <br />;
+
+  const stringMonth = new Intl.DateTimeFormat('en-GB', {
+    month: 'long',
+  }).format(date) || <br />;
 
   return (
     <div className={s.clock}>
@@ -66,17 +80,17 @@ function Clock() {
       <div className={s.buttonsContainer}>
         <SuperButton
           id={'hw9-button-start'}
-          disabled={true} // пишут студенты // задизэйблить если таймер запущен
+          disabled={!!timerId} // пишут студенты // задизэйблить если таймер запущен
           onClick={start}
         >
-          start
+          Start
         </SuperButton>
         <SuperButton
           id={'hw9-button-stop'}
-          disabled={true} // пишут студенты // задизэйблить если таймер не запущен
+          disabled={!timerId} // пишут студенты // задизэйблить если таймер не запущен
           onClick={stop}
         >
-          stop
+          Stop
         </SuperButton>
       </div>
     </div>
